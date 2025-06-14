42dc83fbcb87a553fe6d49f741b9339d014c3b29
codingMaz <m.balandari.toroghi@gmail.com>
2025-06-14 12:09:01 +0330

    add home app with two simple pages

--- project_django/settings.py
+++ project_django/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "home",
 ]
 
 MIDDLEWARE = [
@@ -108,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Asia/Tehran"
 
 USE_I18N = True
 
